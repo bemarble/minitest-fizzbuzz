bf9c15ce2d600365aaa9d44bb042f714c4aecd18
BeMarble <mktkbys@gmail.com>
2017-02-17 01:46:44 +0900

    refacor minitest

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -3,14 +3,28 @@ require 'minitest/autorun'
 
 # FizzBuzzTest
 class FizzBuzzTest < Minitest::Test
-  def test_fizz_buzz
-    fb = FizzBuzz.new
-    assert_equal '1', fb.fizz_buzz(1)
-    assert_equal '2', fb.fizz_buzz(2)
-    assert_equal 'Fizz', fb.fizz_buzz(3)
-    assert_equal '4', fb.fizz_buzz(4)
-    assert_equal 'Buzz', fb.fizz_buzz(5)
-    assert_equal 'Fizz', fb.fizz_buzz(6)
-    assert_equal 'Fizz Buzz', fb.fizz_buzz(15)
+  def setup
+    @fb = FizzBuzz.new
+  end
+
+  def test_fizz_buzz_return_num
+    assert_equal '1', @fb.fizz_buzz(1)
+    assert_equal '2', @fb.fizz_buzz(2)
+    assert_equal '4', @fb.fizz_buzz(4)
+  end
+
+  def test_fizz_buzz_return_fizz
+    assert_equal 'Fizz', @fb.fizz_buzz(3)
+    assert_equal 'Fizz', @fb.fizz_buzz(6)
+  end
+
+  def test_fizz_buzz_return_buzz
+    assert_equal 'Buzz', @fb.fizz_buzz(5)
+    assert_equal 'Buzz', @fb.fizz_buzz(10)
+  end
+
+  def test_fizz_buzz_return_fizz_buzz
+    assert_equal 'Fizz Buzz', @fb.fizz_buzz(15)
+    assert_equal 'Fizz Buzz', @fb.fizz_buzz(30)
   end
 end
